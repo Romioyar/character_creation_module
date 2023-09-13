21e7ae49da95c7a540e68a7e7911de29284ec6a8
Roman Zarubin <romioyar@yandex.ru>
2023-09-13 13:28:10 +0200

    Fixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from grafic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack calculation."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику '
                 f'равный {5 + randint(3, 5)}')
@@ -17,7 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
-
+    """Defence calculation."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -28,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Cpecial attack calculation."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -41,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Function starts training stage."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -65,6 +68,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choose character class."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -88,6 +92,9 @@ def choice_char_class() -> str:
 
 if __name__ == '__main__':
     def main():
+        """Main function to run screendaver all other functions
+        in this module and print main text
+        """
         run_screensaver()
         print('Приветствую тебя, искатель приключений!')
         print('Прежде чем начать игру...')
